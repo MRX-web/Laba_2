748c958d3d5e78d4dc98159fd980342173577fef
MRX-web <57215654+MRX-web@users.noreply.github.com>
2019-10-31 12:52:48 +0300

    Task_2

--- Program.cs
+++ Program.cs
@@ -1,59 +1,61 @@
 ﻿using System;
 
-namespace Task_1
+namespace Task_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int i = 1;
-			while (i == 1){
+            int k = 1;
+            Console.WriteLine("Введите количество слогаемых:\n");
+            while (k == 1)
+            {
+                try
+                {
 
-                try{
-                Console.WriteLine("Решение квадратного уравнения\n");
+                    int n = Convert.ToInt32(Console.ReadLine());
 
-                    Console.WriteLine("Введите число A:");
-                        double a = Double.Parse(Console.ReadLine());
+                    double j = 1;
 
-                            Console.WriteLine("Введите число B:");
-                                double b = Double.Parse(Console.ReadLine());
+                    double pi = 0;
+                    int i = 0;
+                    k++;
+                    while (i < n)
+                    {
 
-                                Console.WriteLine("Введите число C:");
-                                    double c = Double.Parse(Console.ReadLine());
-                    i++;
-                        //формула для нахождения дискриминанта
-                        double D = Math.Sqrt(Math.Pow(b, 2) - 4 * a * c);
+                        double sum = 1 / j;
+                        j += 2;
+                        
 
-                            double X1 = (-b - D) / (2 * a);
-                                double X2 = (-b + D) / (2 * a);
-                                    double X3 = (-b) / (2 * a);
-               
-                    if(D > 0){
+                        double sum1 = -1 / j;
+                        j += 2;
 
-                        Console.WriteLine("Уравнение имеет два корня x1:{0} /n x2:{1}", X1, X2);
-                 
-                    }
-                        else if (D == 0){
+                        
+                        pi = pi + sum + sum1;
+                        i += 2;
 
-                            Console.WriteLine("Уравнение имеет один корень x3:" + X3);
-
+                        if (i > n) { 
+                            pi -= sum1; 
                         }
-                    else{
-
-                        Console.WriteLine("Уравнение не имеет действительный корней!");
-                            Console.WriteLine("Вывод: x+iy; x-iy");
-                                Console.WriteLine(Math.Pow(a, 2) - Math.Pow(b, 2) * (-1));
-
+                    
                     }
 
-                
+                    pi = pi * 4;
+
+                    Console.WriteLine("Приблеженное число П:\n" + pi);
+
+
                 }
-            catch(FormatException){
-                Console.WriteLine("Введите корректное число:");
-                    i = 1;
+
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введи корректное число:");
+                    k = 1;
+
+                }
+
             }
-        } 
-                Console.ReadKey();  
+            Console.ReadKey();
         }
     }
 }
