a107cb42a5b3f5b42da568409fe074b75b75e2db
MRX-web <57215654+MRX-web@users.noreply.github.com>
2019-10-31 12:49:54 +0300

    Ind_2

--- Program.cs
+++ Program.cs
@@ -1,53 +1,45 @@
 ﻿using System;
 
-namespace Ind_1
+namespace Ind_2
 {
     class Program
     {
         static void Main(string[] args)
         {
             int i = 1;
-            Console.WriteLine("Введите вещественное число X:");
-            double X = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Введите вещественное число Y:");
-            double Y = Convert.ToDouble(Console.ReadLine());
-            while (i == 1)
-            {
-                try
-                {
-
-                    if (X < 0 || Y < 0)
+            while (i == 1) {
+                try {
+                    Console.WriteLine("Введите число N 1 > N < 10000");
+                    int N = Convert.ToInt32(Console.ReadLine());
+                    if (N > 1 && N < 10000)
                     {
-                        Console.WriteLine("Error!!!");
+                        Console.WriteLine("Нечетные делители числа: ");
+                        for (int k = 1; k <= N; k = k + 2) {
+
+                            if (N % k == 0)
+                            {
+                                Console.WriteLine(k + " ");
+                            }
+           
+                        }
+
                         i++;
-                    }
-                    else if (X < Y)
-                    {
-                        X = Math.Sqrt(X);
-                        Console.WriteLine("Квадратный корень меньшего числа: " + X);
-                    }
-                    else if (Y < X)
-                    {
-                        Y = Math.Sqrt(Y);
-                        Console.WriteLine("Квадратный корень меньшего числа: " + Y);
-
+                       
                     }
                     else
                     {
-                        Console.WriteLine("Введиет корректное значение:");
+                        Console.WriteLine("Введите корректное число");
                         i = 1;
                     }
-                    i++;
                 }
+
                 catch (FormatException)
                 {
-
-                    Console.WriteLine("Введиет корректное значение:");
+                    Console.WriteLine("Введите корректное число");
                     i = 1;
                 }
             }
             Console.ReadKey();
         }
-    }
+    } 
 }
