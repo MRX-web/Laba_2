50bf17a6f8df4d2d4d09baa866de11a68fc18267
MRX-web <57215654+MRX-web@users.noreply.github.com>
2019-10-31 12:51:24 +0300

    Task_1

--- Program.cs
+++ Program.cs
@@ -1,45 +1,59 @@
 ﻿using System;
 
-namespace Ind_2
+namespace Task_1
 {
     class Program
     {
         static void Main(string[] args)
         {
             int i = 1;
-            while (i == 1) {
-                try {
-                    Console.WriteLine("Введите число N 1 > N < 10000");
-                    int N = Convert.ToInt32(Console.ReadLine());
-                    if (N > 1 && N < 10000)
-                    {
-                        Console.WriteLine("Нечетные делители числа: ");
-                        for (int k = 1; k <= N; k = k + 2) {
+			while (i == 1){
 
-                            if (N % k == 0)
-                            {
-                                Console.WriteLine(k + " ");
-                            }
-           
+                try{
+                Console.WriteLine("Решение квадратного уравнения\n");
+
+                    Console.WriteLine("Введите число A:");
+                        double a = Double.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Введите число B:");
+                                double b = Double.Parse(Console.ReadLine());
+
+                                Console.WriteLine("Введите число C:");
+                                    double c = Double.Parse(Console.ReadLine());
+                    i++;
+                        //формула для нахождения дискриминанта
+                        double D = Math.Sqrt(Math.Pow(b, 2) - 4 * a * c);
+
+                            double X1 = (-b - D) / (2 * a);
+                                double X2 = (-b + D) / (2 * a);
+                                    double X3 = (-b) / (2 * a);
+               
+                    if(D > 0){
+
+                        Console.WriteLine("Уравнение имеет два корня x1:{0} /n x2:{1}", X1, X2);
+                 
+                    }
+                        else if (D == 0){
+
+                            Console.WriteLine("Уравнение имеет один корень x3:" + X3);
+
                         }
+                    else{
 
-                        i++;
-                       
+                        Console.WriteLine("Уравнение не имеет действительный корней!");
+                            Console.WriteLine("Вывод: x+iy; x-iy");
+                                Console.WriteLine(Math.Pow(a, 2) - Math.Pow(b, 2) * (-1));
+
                     }
-                    else
-                    {
-                        Console.WriteLine("Введите корректное число");
-                        i = 1;
-                    }
+
+                
                 }
-
-                catch (FormatException)
-                {
-                    Console.WriteLine("Введите корректное число");
+            catch(FormatException){
+                Console.WriteLine("Введите корректное число:");
                     i = 1;
-                }
             }
-            Console.ReadKey();
+        } 
+                Console.ReadKey();  
         }
-    } 
+    }
 }
